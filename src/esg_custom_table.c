f66a72030224d635605ec07796c06b6fde486d97
fanglinghe <523766410@qq.com>
2017-06-02 14:34:30 +0800

    update
    update

--- src/esg_custom_table.c
+++ src/esg_custom_table.c
@@ -3,11 +3,24 @@ cus_table_t tal3; //extern
 
 
 
-
-int initCol(cus_col_t *col, name, type, ....)
+int initCol(cus_col_t *col, char *name, int type, int len, int precision, int scale, char *min, char *max, long long seq)
 {
-	
-	
+	strcpy(col->col_name, name);
+    col->type = type;
+    col->length = len;
+	col->precision = precision;
+    col->scale = scale;
+	if (NULL != min)
+	{
+		strcpy(col->min, min);
+	}
+
+	if (NULL != max)
+	{
+		strcpy(col->max, max);
+	}
+	col->seq = seq;
+
 }
 
 
@@ -26,10 +39,13 @@ void printCol(cus_col_t *col)
 }
 
 
-void genData(cus_table_t *tal)
+void genData(cus_table_t *tab)
 {
-	
-	for (i = 0; i < tal.col_num; i++)
+	for (n = 0; n < tab.row_num; n++)
+	{
+		
+	}
+	for (i = 0; i < tab.col_num; i++)
 	{
 		printCol()
 		printTerm()
@@ -55,6 +71,13 @@ cus_table_t* genTable()
 	cus_table_t *tal = malloc(sizeof(cus_table_t)); //heap
 	cus_table_t tal2; //stack, error
 	tal3; //global
+	
+	
+	tal->cols = malloc(tal->col_num * sizeof(cus_col_t));
+	memset(tal->cols, 0, len);
+	
+	initCol(tab->cols, "AA", CUS_INT, 0, 0, 0, "1", "1000", 0);
+	initCol(tab->cols + 1, "BB", CUS_INT, 0, 0, 0, NULL, NULL, 123);
 	
 }
 #endif
--- src/esg_custom_table.h
+++ src/esg_custom_table.h
@@ -4,20 +4,44 @@
 #define STREAMS_H
 
 
+#define CUS_NAME_LEN  64
+#define CUS_NUM_LEN   128
+
 enum cus_types
 {
 	CUS_INT,
 	CUS_CHAR,
-	
+	CUS_DECIMAL,
+	CUS_DATE,
+	CUS_TIME,
+	CUS_TIMESTAMP,
+	CUS_RANDOM,
+	CUS_EMAIL,
+	CUS_INT_YEAR,
+	CUS_INT_MONTH,
+	CUS_INT_DAY,
+	CUS_INT_HOUR,
+	CUS_INT_MINUTE,
+	CUS_INT_SECOND,
+	CUS_INT_YM, //INTERVAL TYPE FOR YEAR TO MONTH
+	CUS_INT_MD, //INTERVAL TYPE FOR MONTH TO DAY
+	CUS_INT_DH, //INTERVAL TYPE FOR DAY TO HOUR
+	CUS_INT_HM, //INTERVAL TYPE FOR HOUR TO MINUTE
+	CUS_INT_MS //INTERVAL TYPE FOR MINUTE TO SECOND
 	
 	
 };
 
 typedef struct CUS_COLUMN
 {
-	char col_name[];
-	int type;
-	
+	char col_name[CUS_NAME_LEN];
+	long long type;
+	long long length;
+	long long precision;
+	long long scale;
+	char min[CUS_NUM_LEN];
+	char max[CUS_NUM_LEN];
+	long long seq ;
 	
 } cus_col_t;
 
@@ -25,9 +49,9 @@ typedef struct CUS_COLUMN
 
 typedef struct CUS_TABLE
 {
-	char tal_name[];
+	char tal_name[CUS_NAME_LEN];
 	int col_num;
-	cus_col_t *cols; //alloc(col_num * sizeof(cus_col_t))
+	cus_col_t *cols; // = malloc(col_num * sizeof(cus_col_t))
 	
 } cus_table_t;
 
